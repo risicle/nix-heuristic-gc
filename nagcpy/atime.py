6dde363283dd89c3b8c2226cc1e4408521303647
Robert Scott <code@humanleg.org.uk>
2023-03-13 00:30:08 +0000

    atime scanning: handle non-dir base paths too

--- nagcpy/atime.py
+++ nagcpy/atime.py
@@ -1,5 +1,6 @@
 from itertools import chain
-from os import scandir
+from os import scandir, stat
+from stat import S_ISDIR
 
 def direntry_max_atime(direntry):
     try:
@@ -7,16 +8,24 @@ def direntry_max_atime(direntry):
             # we are not interested in the atime of directories
             # themselves because we ourselves affect them by
             # walking them
-            return path_max_atime(direntry.path)
+            return dir_max_atime(direntry.path)
 
         return direntry.stat(follow_symlinks=False).st_atime
     except PermissionError:
         return 0
 
 
-def path_max_atime(path):
+def dir_max_atime(path):
     return max(chain(
         (direntry_max_atime(direntry) for direntry in scandir(path)),
         # avoid max() being fed an empty sequence
         (0,),
     ))
+
+
+def path_max_atime(path):
+    s = stat(path, follow_symlinks=False)
+    if S_ISDIR(s.st_mode):
+        return dir_max_atime(path)
+
+    return s.st_atime
