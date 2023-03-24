8664deef0e54ac19e7368390e52b7bfbc212c663
Robert Scott <code@humanleg.org.uk>
2023-03-24 23:14:18 +0000

    fs module: add typing

--- nagcpy/fs.py
+++ nagcpy/fs.py
@@ -1,9 +1,13 @@
 from functools import reduce
 from itertools import chain
-from os import scandir, stat
+from os import scandir, stat, DirEntry
 from stat import S_ISDIR
 
-def direntry_stat_agg(direntry):
+
+AggStatTuple = tuple[int,int]
+
+
+def direntry_stat_agg(direntry:DirEntry) -> AggStatTuple:
     try:
         if direntry.is_dir(follow_symlinks=False):
             # we are not interested in the atime of directories
@@ -16,13 +20,13 @@ def direntry_stat_agg(direntry):
         return 0, 1
 
 
-def _stat_agg_reduction(a, b):
+def _stat_agg_reduction(a:AggStatTuple, b:AggStatTuple) -> AggStatTuple:
     atime_a, inodes_a = a
     atime_b, inodes_b = b
     return max(atime_a, atime_b), inodes_a + inodes_b
 
 
-def dir_stat_agg(path):
+def dir_stat_agg(path:str) -> AggStatTuple:
     return reduce(
         _stat_agg_reduction,
         (direntry_stat_agg(direntry) for direntry in scandir(path)),
@@ -30,7 +34,7 @@ def dir_stat_agg(path):
     )
 
 
-def path_stat_agg(path):
+def path_stat_agg(path:str) -> AggStatTuple:
     s = stat(path, follow_symlinks=False)
     if S_ISDIR(s.st_mode):
         return dir_stat_agg(path)
