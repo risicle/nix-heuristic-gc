151b29f609b4b4a7d66b97d6b8e96bb8a6471bc0
Robert Scott <code@humanleg.org.uk>
2023-03-24 23:14:42 +0000

    add --verbose and --quiet flags

--- nagcpy/__main__.py
+++ nagcpy/__main__.py
@@ -5,11 +5,6 @@ def main():
     from humanfriendly import parse_size
     from nagcpy import nix_heuristic_gc
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s:%(levelname)s:%(name)s: %(message)s",
-    )
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--penalize-drvs", default=True, action=argparse.BooleanOptionalAction)
     parser.add_argument("--penalize-substitutable", default=True, action=argparse.BooleanOptionalAction)
@@ -17,11 +12,21 @@ def main():
     parser.add_argument("--penalize-size", default=False, action=argparse.BooleanOptionalAction)
     parser.add_argument("--penalize-exceeding-limit", default=True, action=argparse.BooleanOptionalAction)
     parser.add_argument("--dry-run", default=False, action=argparse.BooleanOptionalAction)
+
+    loglvl_grp = parser.add_mutually_exclusive_group()
+    loglvl_grp.add_argument("--verbose", "-v", dest="loglevel", action="store_const", const=logging.DEBUG)
+    loglvl_grp.add_argument("--quiet", "-q", dest="loglevel", action="store_const", const=logging.WARNING)
+
     parser.add_argument("reclaim_bytes")
 
     parsed = vars(parser.parse_args())
 
     parsed["reclaim_bytes"] = parse_size(parsed["reclaim_bytes"])
+
+    logging.basicConfig(
+        level=parsed.pop("loglevel", logging.INFO),
+        format="%(asctime)s:%(levelname)s:%(name)s: %(message)s",
+    )
 
     nix_heuristic_gc(**parsed)
 
